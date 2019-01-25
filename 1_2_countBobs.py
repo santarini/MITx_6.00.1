b01e83bb133ca4a819e11abb8f12fcdbcb493b39
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-24 15:58:30 -1000

    Update 1_2_countBobs.py

--- 1_2_countBobs.py
+++ 1_2_countBobs.py
@@ -1,4 +1,11 @@
-s = 'azcbobobegghakl'
-print("Number of times bob occurs is: " + str(s.count('bob')))
+bobs = 0
+for i in range (0,len(s)):
+    if s[i]=="b":
+        if s[i+1]=="o":
+            if s[i+2]=="b":
+                bobs +=1
+    i += 1
+print("Number of times bob occurs is: " + str())
+
 
 #not working. Doesn't count bobob as two bobs.....
