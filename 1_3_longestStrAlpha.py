aeea4c3f8549f2c576d8c9f6f2baf200609a3d45
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-24 16:34:26 -1000

    Update 1_3_longestStrAlpha.py

--- 1_3_longestStrAlpha.py
+++ 1_3_longestStrAlpha.py
@@ -1,1 +1,20 @@
-
+s = 'azcbobobegghakl'
+alphabet = [chr(i) for i in range(ord('a'),ord('z')+1)]
+for letterInStr in range(0,len(s)):
+    #take in a letter from a string
+    s[letterInStr]
+    #get letter's numeric index reference in alphabet list
+    alphabetStart = alphabet.index(s[letterInStr])
+    print(alphabetStart)
+    #if the letter is Z next letter.
+    if alphabetStart == 25:
+        continue
+    #if the next letter in the string is equal to the next letter index of alphabet reference
+    if s[letterInStr + 1] == alphabet[alphabetStart + 1]:
+        print("YEP")
+        #increment counter
+        #concat character to stringplace holder
+        #reset index reference
+        #check again
+        #once cycle is broken push final string to list
+    #return string of greatest length
