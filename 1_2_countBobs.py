196b1233e8cddc7b2becfe6fac8b38fd0bdc9260
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-24 16:02:41 -1000

    Update 1_2_countBobs.py

--- 1_2_countBobs.py
+++ 1_2_countBobs.py
@@ -1,11 +1,11 @@
 bobs = 0
-for i in range (0,len(s)):
+for i in range (0,len(s)-2):
     if s[i]=="b":
         if s[i+1]=="o":
             if s[i+2]=="b":
                 bobs +=1
     i += 1
-print("Number of times bob occurs is: " + str())
+print("Number of times bob occurs is: " + str(bobs))
 
 
-#not working. Doesn't count bobob as two bobs.....
+#passed
