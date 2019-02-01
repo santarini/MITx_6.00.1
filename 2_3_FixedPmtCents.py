7d3eea3505d14ba52b66d1fcb84ea833fbdefd17
Makoa <32736507+santarini@users.noreply.github.com>
2019-01-31 18:01:07 -1000

    Update 2_3_FixedPmtCents.py

--- 2_3_FixedPmtCents.py
+++ 2_3_FixedPmtCents.py
@@ -5,30 +5,43 @@ annualInterestRate = 0.2
 #define some preliminary stuff
 totalPaid = 0
 monthlyInterestRate = annualInterestRate/12.0
-monthlyPmtLower = balance / 12
+
+#define some bounds
+lowerBound = balance / 12
 ear = (1 + monthlyInterestRate)**12
-monthlyPmtUpper = (balance * ear)/12.0
+upperBound = (balance * ear)/12.0
 
 #figure out the 12 month ending compounded balance
 for i in range(1,13):
     balance = balance * (1+monthlyInterestRate)
 
-#figure out the distance of the bounds
-distanceBetweenBounds = monthlyPmtUpper - monthlyPmtLower
-#find the mid point between them
-middleOfBounds = distanceBetweenBounds/2
-#get the nubmer in the middle of them
-pmtInMiddle = monthlyPmtLower + middleOfBounds
-#set that equal to our fixed pmt
-fixedPayment = pmtInMiddle
-for i in range(1,13):
-    totalPaid = totalPaid + fixedPayment
-    totalPaid = totalPaid * (1+monthlyInterestRate)
-if totalPaid < balance:
-    #set lower bound to pmtInMiddle and reset
-if pmtInMiddle > balance:
-    #set upper bound to pmtInMiddle and reset
+while totalPaid != balance:
+    #reset total paid
+    totalPaid = 0
+    #figure out the distance of the bounds
+    distanceBetweenBounds = upperBound - lowerBound
+    print("Upper: " +str(upperBound) + " Lower: " + str(lowerBound))
+    print("Distance between bounds " +str(distanceBetweenBounds))
+    #find the mid point between them
+    middleOfBounds = distanceBetweenBounds/2
+    print("Mid of bounds " +str(middleOfBounds))
+    #get the nubmer in the middle of them
+    pmtInMiddle = lowerBound + middleOfBounds
+    print("Mid of bounds " +str(pmtInMiddle))
+    #set that equal to our fixed pmt
+    fixedPayment = pmtInMiddle
+    print("Fixed Payment " +str(fixedPayment))
+    for i in range(1,13):
+        totalPaid = totalPaid + fixedPayment
+        totalPaid = totalPaid * (1+monthlyInterestRate)
+    if totalPaid < balance:
+        #set lower bound to pmtInMiddle and reset
+        lowerBound = fixedPayment
+    if totalPaid > balance:
+        #set upper bound to pmtInMiddle and reset
+        upperBound = fixedPayment
+    if totalPaid == balance:
+        print(fixedPayment)
 
 
-print(pmtInMiddle)
-
+#print(pmtInMiddle)
